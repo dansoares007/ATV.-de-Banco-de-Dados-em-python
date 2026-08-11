0d865779eff2f183a42588a2bd7d636031ca055b
Daniel Soares <danielsantosmarinho402@gmail.com>
2026-08-11 01:57:13 -0300

    Organizando os commits

--- Atividade.py
+++ Atividade.py
@@ -1,1 +1,20 @@
-print ("Bom dia, boa tarde ou boa noite!!!")
+class Carro :
+
+    def __init__(self, marca, velocidade, placa, cor) :
+        self.marca = marca
+        self.velocidade = velocidade
+        self.placa = placa
+        self.cor = cor
+
+    def get_marca(self) :
+        return self.marca
+    
+    def get_velocidade(self) :
+        return self.velocidade
+
+    def get_placa(self) :
+        return self.placa
+
+    def get_cor(self) :
+        return self.cor
+    
---
+++ arquivo.py
@@ -0,0 +1,10 @@
+from Atividade import Carro
+
+resposta = input(print("Você quer adicionar um carro ao arquivo? "))
+
+while resposta == 'sim' :
+
+    hotwhells = Carro(input(print("Pra adicionar o marca")), input(print("Pra adicionar a velocidade: ")), input(print("Pra adicionar a placa: ")), input(print("Pra adicionar a cor: ")))
+
+    
+
